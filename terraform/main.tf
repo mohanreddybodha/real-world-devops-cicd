a5b15a48d94628be443220b97b009520f8462362
mohanreddy <bodha@gmail.com>
2026-02-08 11:11:48 +0530

    modified docker file

--- terraform/main.tf
+++ terraform/main.tf
@@ -75,7 +75,7 @@ resource "aws_security_group" "k8s_sg" {
     from_port   = 6443
     to_port     = 6443
     protocol    = "tcp"
-    cidr_blocks = [var.admin_ip_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
